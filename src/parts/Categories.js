7d033ed28ef90aeba5a15e8139e12b18f71ca562
ichwanrizky <77712459+ichwanrizky@users.noreply.github.com>
2023-02-20 23:31:02 +0700

    Categories

--- src/parts/Categories.js
+++ src/parts/Categories.js
@@ -1,52 +1,56 @@
 import React from 'react'
 import ContohGambar from "assets/images/img-mostpick1.jpg";
+import Button from 'elements/Button';
 
 export default function Categories(props) {
 
     return(
         props.data.map((item, index) => {
             return (
-                <div className='container section-categories' key={`categories-${index}`}>
+                <section className='container' key={`category-${index}`}>
+
                     <h4 className='mb-3'>{item.name}</h4>
 
                     <div className='container-grid'>
+
                         {
                             item.items.map((item2, index2) => {
                                 return(
-                                    <div className='item column-3 row-1'>
-                                        <div className='card card-featured'>
+
+                                    <div className='item column-3 row-1' key={`category-item-${index2}`}>
+                                        <div className='card'>
                                             {
-                                                item2.isPopular ? 
-                                                <>
-                                                <div className='tag'>
-                                                    Popular
-                                                    <span className='font-weight-light'> Choice </span>
-                                                </div>
-                                                </> : ""
+                                                item2.isPopular && (
+                                                    <div className='tag'>
+                                                        Popular
+                                                        <span className='font-weight-light'> choice</span>
+                                                    </div>
+                                                )
                                             }
-                                            
+
                                             <figure className='img-wrapper'>
-                                                <img src={ContohGambar} alt='' className="img-cover"></img>
+                                                <img src={ContohGambar} className='img-cover' style={{height: 180}}></img>
                                             </figure>
 
-                                        </div>
-
-                                        <h6 className='mt-3'>
-                                            {item2.name} 
-                                            <div>
-                                                <span className='text-gray-500 font-weight-light'>{item2.city}, {item2.country}</span>
+                                            <div className='meta-wrapper'>
+                                                <Button type='link' className='streched-link d-block text-gray-800' href=''>
+                                                    <h5>{item2.name}</h5>
+                                                </Button>
+                                                <span className='text-gray-500'>
+                                                    {item2.city}, {item2.country}
+                                                </span>
                                             </div>
-                                        </h6>
+                                        </div>
                                     </div>
 
-                                    
                                 )
                             })
                         }
 
+
                     </div>
 
-                </div>
+                </section>
             );
         })
     )
